a5298889e2f1ac6f58c25d5cffea568b54aa5480
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2022-04-23 16:26:53 +0300

    Add files via upload

---
+++ vasd.java
@@ -0,0 +1,13 @@
+import WeightImpact(sort,0)
+PublicClassFrameScheme(0);
+		Add value FrameClass; 
+	{	 
+			chart to
+	AddValue1(0)ASCII to Document.Classs
+	Weight.SuspendedAlphabetSpiral(0) 
+	NULL 
+			AddFrameSchemeLock.Reader;
+
+	to
+	PublicAddedValue1(0)ASCII
+}
